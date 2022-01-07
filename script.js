338759a910e57e928afac5abff4c50c1abca6ca1
dnlespinosa <89487129+dnlespinosa@users.noreply.github.com>
2022-01-06 18:01:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+
+// Whats the difference between var and let? Var can be reassigned and redeclared. Let cannot be redeclared. 
+// Whats the difference between var and const? Var can be reassigned and redeclared. Const cannot be redeclared or reassigned. 
+// Whats the difference between let and const? Let can be reassigned, const cannot
+// Hoisting is trying to use variables before they are declared
